0e53581ce23c95602855786a788168ebbe65f0f8
sungroo <sungro@hotmail.com>
2026-01-13 18:16:03 -0500

    fix: add missing next.config.mjs and simplify toml

---
+++ paper-rings-inc/next.config.mjs
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* This helps Netlify find the right paths in a subdirectory */
+  distDir: '.next',
+};
+
+export default nextConfig;
